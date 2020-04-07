4090f8bbdeb03d43347f2f347c298949c4c20d17
zee <zeeshiq58@gmail.com>
2020-04-08 01:15:12 +0500

    created Templates folder form html files

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
-    
-
+	url('', views.home,name='blog-home'),
     url('about/', views.about, name='blog-about'),
-    url('', views.home,name='blog-home'),
 
 ]
