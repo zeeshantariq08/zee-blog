7fcfc01c5c5dd5d3ae8a50ba50b24f3f9ab67a2d
zee <zeeshiq58@gmail.com>
2020-04-08 01:04:04 +0500

    add blog app and set routes for them

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+
+    
+
+    url('about/', views.about, name='blog-about'),
+    url('', views.home,name='blog-home'),
+
+]
---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+	return HttpResponse('Blog Home')
+
+
+
+def about(request):
+	return HttpResponse('Blog About')
+
+
